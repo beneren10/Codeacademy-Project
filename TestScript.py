78527dc0d6d8a66d10ca4010f4eeb132693418ea
Benjamin Eren <Ben@sumcoeu.com>
2024-09-22 18:01:18 +0100

    added comment

--- TestScript.py
+++ TestScript.py
@@ -3,9 +3,7 @@ def rangeFunction(a,b):
 
 print(rangeFunction(1,5))
 
-<<<<<<< HEAD
 for i in range(1,1000):
     pass
-=======
-x = "Hello I want to commit"
->>>>>>> 80f4754819fe071ff2f5615531219ab21eb1c6b3
+
+# test addition to this project. 
