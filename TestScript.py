ae88731cc45aac6d92f26c5108fc47c9ccc77c86
Benjamin Eren <Ben@sumcoeu.com>
2024-09-22 18:04:13 +0100

    hello this is VSCODE

--- TestScript.py
+++ TestScript.py
@@ -3,7 +3,9 @@ def rangeFunction(a,b):
 
 print(rangeFunction(1,5))
 
-for i in range(1,1000):
-    pass
+for i in range(1,1000,100):
+    print(i)
 
 # test addition to this project. 
+
+# hello test for vscode
