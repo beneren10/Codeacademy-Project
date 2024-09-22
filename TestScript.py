56fe3a555ac94109c2ea3ebf78f00653886bbbae
Benjamin Eren <Ben@sumcoeu.com>
2024-09-22 18:23:07 +0100

    I wrote the foundation code for RPS untested

--- TestScript.py
+++ TestScript.py
@@ -1,2 +1,42 @@
 #rock paper scissors programme written using GIT hub 
 
+import random
+
+def rps(userChoice):
+    
+    def computerInput():
+        computerChoice = random.randint(0,2)
+        match computerChoice: 
+            case 0:
+                return "r"
+            case 1:
+                return "p"
+            case 2:
+                return "s"
+            
+    def userInput():
+        compChoice = computerInput()
+
+        if compChoice == userChoice:
+            return "Draw"
+        elif compChoice == "r":
+            match userChoice:
+                case "p":
+                    return "{} v {} - User Wins".format(compChoice,userChoice)
+                case "s":
+                    return "{} v {} - Computer Wins".format(compChoice,userChoice)
+        elif compChoice == "p":
+            match userChoice:
+                case "r":
+                    return "{} v {} - Computer Wins".format(compChoice,userChoice)
+                case "s":
+                    return "{} v {} - User Wins".format(compChoice,userChoice)
+        elif compChoice == "s":
+            match userChoice:
+                case "p":
+                    return "{} v {} - Computer Wins".format(compChoice,userChoice)
+                case "r":
+                    return "{} v {} - User Wins".format(compChoice,userChoice)
+    
+
+        
